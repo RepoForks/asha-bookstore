a9fc1121afb955cf42c4c367c29100eea7e2f2fd
tiagohm <tiago.henrique.cco@gmail.com>
2017-06-07 16:21:41 -0300

    Criar a tela ProductPage

---
+++ ProductPage.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ashaBookStore</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/uikit.min.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <script defer src="js/uikit.min.js"></script>
+    <script defer src="js/uikit-icons.min.js"></script>
+  </head>
+  <body class="fundo-amarelo">
+      <div class="container">
+
+        <header class="row">
+          <div class="pull-left">
+            <img class="logo" src="img/logo.png" style="width: 100px" />
+          </div>
+        <div class="pull-right">
+          <a href="#" class="btn btn-info btn-lg">
+            <span class="glyphicon glyphicon-shopping-cart"></span>
+          </a>
+          <a href="#" class="btn btn-success btn-lg">
+            <span class="glyphicon glyphicon-user"></span>
+          </a>
+        </div>
+        </header>
+
+        <div class="body row">
+          <div class="col-md-3">
+            <div class="navegacao">
+
+            </div>
+          </div>
+
+          <div class="col-md-9">
+            <div class="busca">
+              <form class="uk-search">
+                <input class="uk-search-input barra-pesquisa" type="search" placeholder="Buscar...">
+            </form>
+            </div>
+
+            <div class="resultados">
+                <div class="item-max"> </div>
+            </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <footer>
+            <div class="text-center uk-padding-small">Todas as regras e promoções são válidas apenas para produtos vendidos e entregues pela ashaBookStore.com. O preço válido será o da finalização da compra. Havendo divergência, prevalecerá o menor preço ofertado.</div>
+            <div class="text-center uk-padding-small"><a href="#">we.are.asha@gmail.com</a></div>
+            <div class="text-center uk-padding-small"><a href="https://github.com/tiagohm/asha-bookstore"><img src="img/github.png" style="width:32px"></img></a></div>
+        </footer>
+      </div>
+  </body>
+</html>
