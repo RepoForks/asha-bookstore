9a2b253447c227d069e96ad616b587a1973f9bd1
tiagohm <tiago.henrique.cco@gmail.com>
2017-06-07 17:50:05 -0300

    Criar a tela Checkout3

---
+++ Checkout3.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ashaBookStore</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/uikit.min.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <script defer src="js/uikit.min.js"></script>
+    <script defer src="js/uikit-icons.min.js"></script>
+  </head>
+  <body class="fundo-amarelo">
+      <div class="container">
+
+        <header class="row">
+          <div class="pull-left">
+            <img class="logo" src="img/logo.png" />
+          </div>
+        <div class="pull-right">
+          <a href="#" class="uk-button uk-button-default">
+            <span class="glyphicon glyphicon-shopping-cart"></span>
+          </a>
+          <a href="#" class="uk-button uk-button-default">
+            <span class="glyphicon glyphicon-user"></span>
+          </a>
+        </div>
+        </header>
+
+        <div class="body row">
+          <div class="col-md-6 col-md-offset-3">
+              <div class="text-center"><h2>PEDIDO CONFIRMADO: 7874598</h2></div>
+              <div class="uk-padding-small">
+                <div class="text-center"><h4>Endereço de entrega: </h4></div>
+                <div class="text-center"><h5>Rua Gusmão Lobo, 100, Vila Poddis, Itajubá, MG</h5></div>
+              </div>
+              <div class="uk-padding-small">
+                <div class="text-center"><h4>Livros comprados: </h4></div>
+                  <div class="livros-comprados">
+                      <div class="item"> </div>
+                      <div class="item"> </div>
+                      <div class="item"> </div>
+                  </div>
+              </div>              
+          </div>
+        </div>
+
+        <hr/>
+
+        <footer>
+            <div class="text-center uk-padding-small">Todas as regras e promoções são válidas apenas para produtos vendidos e entregues pela ashaBookStore.com. O preço válido será o da finalização da compra. Havendo divergência, prevalecerá o menor preço ofertado.</div>
+            <div class="text-center uk-padding-small"><a href="#">we.are.asha@gmail.com</a></div>
+            <div class="text-center uk-padding-small"><a href="https://github.com/tiagohm/asha-bookstore"><img src="img/github.png" style="width:32px"></img></a></div>
+        </footer>
+      </div>
+  </body>
+</html>
