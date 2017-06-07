3e13fe9ea32aa1a8aa07353b49441a4cbb96f13d
tiagohm <tiago.henrique.cco@gmail.com>
2017-06-07 17:29:54 -0300

    Criar a tela Checkout1

---
+++ Checkout1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ashaBookStore</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/uikit.min.css">
+    <link rel="stylesheet" href="css/theme.css">
+    <script defer src="js/uikit.min.js"></script>
+    <script defer src="js/uikit-icons.min.js"></script>
+  </head>
+  <body class="fundo-amarelo">
+      <div class="container">
+
+        <header class="row">
+          <div class="pull-left">
+            <img class="logo" src="img/logo.png" />
+          </div>
+        <div class="pull-right">
+          <a href="#" class="uk-button uk-button-default">
+            <span class="glyphicon glyphicon-shopping-cart"></span>
+          </a>
+          <a href="#" class="uk-button uk-button-default">
+            <span class="glyphicon glyphicon-user"></span>
+          </a>
+        </div>
+        </header>
+
+        <div class="body row">
+          <div class="col-md-6 col-md-offset-3">
+              <div class="text-center"><h4>Você tem 5 itens em seu carrinho.</h4></div>
+              <div class="text-center"><h4>Insira seu e-mail para continuar a compra:</h4></div>
+              <div class="uk-padding-small">
+                <input class="uk-search-input barra-pesquisa" type="text" placeholder="E-mail"></input>
+              </div>
+              <div class="text-center">
+                <button class="uk-button uk-button-success">Finalizar Compra</button>                
+              </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <footer>
+            <div class="text-center uk-padding-small">Todas as regras e promoções são válidas apenas para produtos vendidos e entregues pela ashaBookStore.com. O preço válido será o da finalização da compra. Havendo divergência, prevalecerá o menor preço ofertado.</div>
+            <div class="text-center uk-padding-small"><a href="#">we.are.asha@gmail.com</a></div>
+            <div class="text-center uk-padding-small"><a href="https://github.com/tiagohm/asha-bookstore"><img src="img/github.png" style="width:32px"></img></a></div>
+        </footer>
+      </div>
+  </body>
+</html>
